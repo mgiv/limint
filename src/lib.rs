ac220a9b6e0ea9954cc9030a3c85145478570ef2
Matthew Given <143132572+bluescorpion35@users.noreply.github.com>
2025-02-13 22:29:36 +0000

    WIP Basic add functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use std::ops::Add;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct ULimInt {
     limit: usize,
     value: usize,
@@ -10,6 +11,13 @@ pub struct ULimInt {
 impl ULimInt {
     pub fn to_int(&self) -> usize {
         self.limit * self.overflow + self.value
+    }
+}
+
+impl Add<ULimInt> for ULimInt {
+    type Output = ULimInt;
+    fn add(self, other: ULimInt) -> ULimInt {
+        (self.true_value + other.true_value).to_limint(self.limit)
     }
 }
 
@@ -36,7 +44,7 @@ impl ToLimInt for usize {
 mod tests {
     use std::process::id; // Use procid for prng (reason below)
 
-    use crate::ToLimInt;
+    use crate::{ToLimInt, ULimInt};
 
     // Quick and dirty prng for testing using xorshift and the procid
     fn cheap_rand(seed: usize) -> usize {
@@ -54,8 +62,8 @@ mod tests {
     fn test_to_limint() {
         let limit = 5usize;
         let value = 10usize;
-        let ulimint = value.to_limint(limit);
-        println!("{:?}", ulimint)
+        let _ulimint = value.to_limint(limit);
+
     }
 
     #[test]
@@ -64,7 +72,6 @@ mod tests {
         let value = cheap_rand(limit);
         let ulimint = value.to_limint(limit);
         assert_eq!(ulimint.true_value, value);
-        println!("{:?}\n\n {}", ulimint, value);
     }
     #[test]
     fn to_int() {
@@ -77,7 +84,22 @@ mod tests {
             let value = cheap_rand(rand);
             let ulimint = value.to_limint(limit);
             assert_eq!(ulimint.true_value, ulimint.to_int());
-            println!("{:?}\n\n {}", ulimint, value);
         }
+    }
+    #[test]
+    fn test_add_limint() {
+        let mut rand = id() as usize;
+        let mut ulimints: Vec<ULimInt> = vec![];
+        for _ in 1..3 {
+            rand = cheap_rand(rand);
+            let limit = cheap_rand(rand);
+            rand = cheap_rand(rand);
+            let value = cheap_rand(rand);
+            ulimints.push(value.to_limint(limit));
+        }
+
+        let resultant = ulimints[0] + ulimints[1];
+        println!("{:?}", resultant);
+
     }
 }
